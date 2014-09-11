19a48a9c03f36f2fab0908fc508b45525740f76b
Matt Oquist <moquist@majen.net>
2014-09-11 13:35:01 -0400

    document for great good

--- bin.pub/timecard.py
+++ bin.pub/timecard.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python
+# This is useful to take my records of the day and turn it into the numbers
+# I need to fill out a timecard.
+#
+# I create the reverse-chronological file by using vim and using :r!date after
+# start: stop: headers.
+#
+# Then I cat that file into this script.
 
-# interpret an input file like this:
+# input like this:
 # stop: Fri Aug 29 20:34:55 EDT 2014
 # start: Fri Aug 29 19:14:52 EDT 2014
+#     - fed the chickens
 # stop: Fri Aug 29 19:04:55 EDT 2014
 # start: Fri Aug 29 18:04:52 EDT 2014
+#    - designed a new space station
+
+# ... yields output like this:
+# Hours worked:  2.335
+# Hours not-worked:  0.165833333333
+# Started:  Fri Aug 29 18:04:52 EDT 2014
+# Ended:  Fri Aug 29 20:34:55 EDT 2014
+#
+# TODO: implement a -r flag to read chronologically-ordered input
 
 import datetime
 import fileinput
