4cd9d907b3805bb9960ed77862f019a075128ea2
Matt Oquist <moquist@majen.net>
2014-11-24 12:12:30 -0500

    keep *inferior-lisp* buffer name

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,7 +58,7 @@
 (defun ensure-clj-repl ()
   "Start a clojure repl using inferior-lisp mode"
   (inferior-lisp "clojure-repl")
-  (rename-buffer "*clj-repl*")
+  ;; (rename-buffer "*clj-repl*") ; Dave experimented with renaming the buffer. I'm experimenting with not doing that.
   (set-syntax-table clojure-mode-syntax-table)
   (clojure-font-lock-setup)
   (supplement-clojure-font-lock))
@@ -66,11 +66,11 @@
 (defun clj-repl ()
   "Switch to existing clojure repl or start a new one"
   (interactive)
-  (let ((repl-window (get-buffer-window "*clj-repl*")))
+  (let ((repl-window (get-buffer-window "*inferior-lisp*")))
     (if repl-window
         (select-window repl-window)
       (split-window nil nil 'left)))
-  (when (switch-to-buffer "*clj-repl*")
+  (when (switch-to-buffer "*inferior-lisp*")
     (rename-buffer "*inferior-lisp*"))
   (ensure-clj-repl))
 
