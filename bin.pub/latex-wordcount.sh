73188f6d3ec6306c778ea1853091919d0d20fa7b
Matt Oquist <moquist@majen.net>
2015-02-10 11:15:38 -0500

    Note location of texcount.pl dependency.

--- bin.pub/latex-wordcount.sh
+++ bin.pub/latex-wordcount.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# texcount.pl is here: http://app.uio.no/ifi/texcount/download.html
+
 wc=$(texcount.pl "$1" | grep 'Words in text' | awk '{print $NF}')
 sed --in-place=noidnoid "s/^Word count:.*\$/Word count: $wc/" "$1"
