07090e302dab8993727caeb44ff0afc884b760e1
Matt Oquist <moquist@majen.net>
2016-03-10 20:16:11 -0500

    Update LaTeX rendering scripts.
    * Fix word-count.
    * Use Bash mo better.

--- bin.pub/latex-wordcount.sh
+++ bin.pub/latex-wordcount.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -ex
+
 # texcount.pl is here: http://app.uio.no/ifi/texcount/download.html
 
 wc=$(texcount.pl "$1" | grep 'Words in text' | awk '{print $NF}')
-sed --in-place=noidnoid "s/^Word count:.*\$/Word count: $wc/" "$1"
+sed --in-place=noidnoid "s/^Word count:.*\$/Word count: $wc/i" "$1"
