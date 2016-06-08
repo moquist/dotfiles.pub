977f5b5253489ec67d996ce87492b55f06a84193
Matt Oquist <moquist@majen.net>
2016-06-08 12:29:11 -0400

    Add dns-update-check.sh

---
+++ bin.pub/dns-update-check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+domain="$1"
+hostname="$2"
+
+function usage {
+    echo "dns-update-check.sh <domain> <host>"
+}
+
+if [ ! "$domain" ]; then
+    echo missing domain
+    usage
+    exit 1
+fi
+
+if [ ! "$hostname" ]; then
+    echo missing hostname
+    usage
+    exit 1
+fi
+
+for server in $(dig +noall +answer NS "$domain" | awk '{print $NF}'); do
+    dig +noall +answer @$server "$hostname"
+done
