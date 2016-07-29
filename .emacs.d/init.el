6636c94a0f5e6792ef3bdb5a3c1bbbeaed41d639
Matt Oquist <moquist@majen.net>
2016-07-29 17:42:22 -0400

    remove adoc-mode from emacs (not found)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,7 +22,6 @@
                        clojure-mode
                        clojure-mode-extra-font-locking
                        better-defaults
-                       adoc-mode
                        paredit))
 (dolist (p package-list)
   (when (not (package-installed-p p))
